95c53d52451904cd3f3eeb2ef7770d38a19b5db6
Gabriel Cardoso <gabrielcarsa@hotmail.com>
2023-09-06 17:20:45 -0400

    Alterando estilo da view Welcome e colocando bootstrap

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,37 +1,54 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    <title>Laravel</title>
 
-        <!-- Styles -->
-        <style>
-         
-        </style>
-    </head>
-    <body class="">
-        <div class="">
-            @if (Route::has('login'))
-                <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
-                    @auth
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter" rel="stylesheet">
+
+
+    <!-- Styles -->
+    <link href="{{ asset('css/welcome.css') }}" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+
+</head>
+
+<body class="">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="{{asset("storage/SmartIPTU.png")}}" width="60px" />
+            </a>
+            <div class="collapse navbar-collapse d-lg-flex justify-content-end" id="navbarText">
+                <span class="navbar-text">
+                    @if (Route::has('login'))
+                    <div class="">
+                        @auth
                         <a href="{{ url('/dashboard') }}" class="">Dashboard</a>
-                    @else
-                        <a href="{{ route('login') }}" class="">Log in</a>
+                        @else
+                        <a href="{{ route('login') }}" class="">Entrar</a>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="">Register</a>
+                        <a href="{{ route('register') }}" class="">Registrar</a>
                         @endif
-                    @endauth
-                </div>
-            @endif
-           
-   
+                        @endauth
+                    </div>
+                    @endif
+                </span>
+            </div>
         </div>
-    </body>
+    </nav>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous">
+    </script>
+</body>
+
 </html>
