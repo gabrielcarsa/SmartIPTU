a1313db3dc1517a94633d3e38d0dacb0456b792f
Gabriel Cardoso <gabrielcarsa@hotmail.com>
2023-09-06 15:43:44 -0400

    Commit inicial do projeto laravel JetStream com pagina welcome vazia

---
+++ resources/views/components/authentication-card-logo.blade.php
@@ -0,0 +1,5 @@
+<x-guest-layout>
+<a href="/">
+    <img src="{{asset("storage/SmartIPTU.png")}}" width="250px"/>
+</a>
+</x-guest-layout>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+        <!-- Styles -->
+        <style>
+         
+        </style>
+    </head>
+    <body class="">
+        <div class="">
+            @if (Route::has('login'))
+                <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
+                    @auth
+                        <a href="{{ url('/dashboard') }}" class="">Dashboard</a>
+                    @else
+                        <a href="{{ route('login') }}" class="">Log in</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}" class="">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+           
+   
+        </div>
+    </body>
+</html>
