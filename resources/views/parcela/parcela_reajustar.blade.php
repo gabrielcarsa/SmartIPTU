b4b32a2347564028be8b09348f4838f79654c820
Gabriel Cardoso <gabrielcarsa@hotmail.com>
2023-11-22 10:26:42 -0500

    reajustando os dois tipos de parcelas a pagar e receber relacionadas a debitos

--- resources/views/parcela/parcela_reajustar.blade.php
+++ resources/views/parcela/parcela_reajustar.blade.php
@@ -25,7 +25,7 @@
 
     <div class="card-body">
         <form class="row g-3"
-            action="{{ isset($parcelaReceberOutros) ? '/contas_receber/definir_reajuste/' . Auth::user()->id : (isset($parcelaPagarOutros) ? '/contas_pagar/definir_reajuste/' . Auth::user()->id : '/parcela/definir_reajuste/' . Auth::user()->id . '?origem=' . request()->input('origem')) }}"
+            action="{{ isset($parcelaReceberOutros) ? '/contas_receber/definir_reajuste/' . Auth::user()->id : (isset($parcelaPagarOutros) ? '/contas_pagar/definir_reajuste/' . Auth::user()->id : '/parcela/definir_reajuste/' . Auth::user()->id . '?origem=' . request()->input('origem').'&lote_id=' . request()->input('lote_id')) }}"
             method="post" autocomplete="off">
             @csrf
             <div class="col-md-4">
