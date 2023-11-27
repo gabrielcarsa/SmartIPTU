afedb45709edffa23ee88ded1568a10825ee9848
Gabriel Cardoso <gabrielcarsa@hotmail.com>
2023-11-27 15:33:37 -0500

    alterando caminho

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,7 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <!-- Styles -->
-    <link href="{{ asset('css/master.css') }}" rel="stylesheet">
+    <link href="{{ asset('html/css/master.css') }}" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
