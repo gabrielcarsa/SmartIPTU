6dd38a6fc50ffea87172425fe77a85928d7b1175
Gabriel Cardoso <gabrielcarsa@hotmail.com>
2023-11-29 11:05:27 -0500

    tentativa arrumar estilo css

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -19,7 +19,7 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <!-- Styles -->
-    <link href="{{ asset('css/master.css') }}" rel="stylesheet">
+    <link href="{{ asset('public_html/css/master.css') }}" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     @livewireStyles
