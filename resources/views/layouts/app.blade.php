f7f5125ee87c86436becee52e875d2860dd9deca
Gabriel Cardoso <gabrielcarsa@hotmail.com>
2023-11-29 12:34:13 -0500

    adicionando Jquery novamente

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -140,8 +140,8 @@
     @stack('modals')
 
     @livewireScripts
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha384-qGi/U7G1pFQlHfP6dJL1Sk52V8HOfL5t6PzCoZFkD5R1RNYT4wGdENL/dzO1QG8b" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
     </script>
