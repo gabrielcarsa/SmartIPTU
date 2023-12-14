a81ef988d28209c8b109d43b487544f64f2d6d90
Gabriel Cardoso <gabrielcarsa@hotmail.com>
2023-12-14 15:40:43 -0500

    organizando logica de exibir parcelas no contas a pagar

--- app/Http/Controllers/ContaPagarController.php
+++ app/Http/Controllers/ContaPagarController.php
@@ -177,7 +177,6 @@ class ContaPagarController extends Controller
         ->join('users as uc', 'uc.id', '=', 'p.cadastrado_usuario_id') // Usuario que cadastrou a parcela
         ->leftJoin('users as ua', 'ua.id', '=', 'p.alterado_usuario_id') // Usuário que alterou, usando LEFT JOIN para permitir nulos
         ->leftJoin('users as ub', 'ub.id', '=', 'p.usuario_baixa_id') // Usuário que baixou, usando LEFT JOIN para permitir nulos
-        ->whereColumn('l.cliente_id', '=', 'td.cliente_id')
         ->orderBy('data_vencimento', 'ASC');
 
 
@@ -221,7 +220,6 @@ class ContaPagarController extends Controller
         FILTRO
         ----------*/
         if ($isReferenteLotes) { //Referente a Lotes
-
             if($titular_conta_id == 0){ //Se o titular da conta for 'Todos'
 
                 if(!empty($periodoDe) && !empty($periodoAte) && $isPeriodoVencimento){ //Verifica período e Vencimento
@@ -355,7 +353,6 @@ class ContaPagarController extends Controller
             'totalValorParcelas' => $totalValorParcelas,
         ];
         
-    
         return view('conta_pagar/contas_pagar', compact('titular_conta', 'data'));
     }
 
