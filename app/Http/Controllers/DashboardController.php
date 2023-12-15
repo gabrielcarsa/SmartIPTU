c3f71ad65365bb72f287c810a8c27770a0e35ee0
Gabriel Cardoso <gabrielcarsa@hotmail.com>
2023-12-15 08:52:56 -0500

    atualizando dados dashboard dos cards valores

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -13,123 +13,42 @@ use App\Models\TipoDebito;
 class DashboardController extends Controller
 {
     function dashboard(){
+        $hoje = now()->toDateString(); // Obtém a data de hoje no formato 'YYYY-MM-DD'
 
-        //Consultas para relacionar debitos e contas a pagar a titulares das contas
-        $debitos_titulares = DB::table('parcela_conta_receber as p')
-           ->selectRaw('tc.id as id')
-           ->selectRaw('SUM(p.valor_parcela) as total_debitos')
-           ->selectRaw('NULL as total_contas_pagar')
-           ->selectRaw('CASE WHEN titular_conta_cliente.razao_social IS NOT NULL THEN titular_conta_cliente.razao_social ELSE titular_conta_cliente.nome END AS nome_cliente_ou_razao_social')
-           ->join('debito as d', 'p.debito_id', '=', 'd.id')
-           ->join('lote as l', 'd.lote_id', '=', 'l.id')
-           ->join('cliente as c', 'l.cliente_id', '=', 'c.id')
-           ->join('titular_conta as tc', 'd.titular_conta_id', '=', 'tc.id')
-           ->leftJoin('cliente AS titular_conta_cliente', 'tc.cliente_id', '=', 'titular_conta_cliente.id')
-           ->whereColumn('l.cliente_id', '=', 'tc.cliente_id')
-           ->whereNull('p.situacao')
-           ->groupBy('tc.id', 'nome_cliente_ou_razao_social');
+        //Soma das parcelas a Pagar no dia de Hoje
+        $pagarHoje =  DB::table('parcela_conta_pagar')
+        ->whereDate('data_vencimento', $hoje)
+        ->where('situacao', 0)
+        ->sum('valor_parcela');
 
+        //Soma das parcelas a Receber no dia de Hoje
+        $receberHoje =  DB::table('parcela_conta_receber')
+        ->whereDate('data_vencimento', $hoje)
+        ->where('situacao', 0)
+        ->sum('valor_parcela');
 
-        $conta_pagar_titulares = DB::table('parcela_conta_pagar as p')
-           ->selectRaw('tc.id as id')
-           ->selectRaw('NULL as total_debitos')
-           ->selectRaw('SUM(p.valor_parcela) as total_contas_pagar')
-           ->selectRaw('CASE WHEN titular_conta_cliente.razao_social IS NOT NULL THEN titular_conta_cliente.razao_social ELSE titular_conta_cliente.nome END AS nome_cliente_ou_razao_social')
-           ->join('conta_pagar as cp', 'p.conta_pagar_id', '=', 'cp.id')
-           ->join('titular_conta as tc', 'cp.titular_conta_id', '=', 'tc.id')
-           ->leftJoin('cliente AS titular_conta_cliente', 'tc.cliente_id', '=', 'titular_conta_cliente.id')
-           ->whereNull('p.situacao')
-           ->groupBy('tc.id', 'nome_cliente_ou_razao_social');
+        //Total de parcelas a pagar atrasadas
+        $pagamentosAtrasados =  DB::table('parcela_conta_pagar')
+        ->whereDate('data_vencimento', '<', $hoje)
+        ->where('situacao', 0)
+        ->sum('valor_parcela');
 
-        //Unindo resultados
-        $titulares_contas = $debitos_titulares->union($conta_pagar_titulares)->get();
-        $data = [];
-        foreach ($titulares_contas as $titular) {
-            $existingTitularKey = null;
-        
-            foreach ($data as $key => $item) {
-                if ($item['id'] == $titular->id) {
-                    $existingTitularKey = $key;
-                    break;
-                }
-            }
-        
-            if ($existingTitularKey !== null) {
-                // Se o titular já existe, verifique e preencha os campos vazios
-                if ($titular->total_debitos !== null && $data[$existingTitularKey]['total_debitos'] === null) {
-                    $data[$existingTitularKey]['total_debitos'] = $titular->total_debitos;
-                }
-                if ($titular->total_contas_pagar !== null && $data[$existingTitularKey]['total_contas_pagar'] === null) {
-                    $data[$existingTitularKey]['total_contas_pagar'] = $titular->total_contas_pagar;
-                }
-            } else {
-                // Se o titular não existe, adicione-o ao array $data
-                $data[] = [
-                    'id' => $titular->id,
-                    'total_debitos' => $titular->total_debitos,
-                    'total_contas_pagar' => $titular->total_contas_pagar,
-                    'nome_cliente_ou_razao_social' => $titular->nome_cliente_ou_razao_social,
-                ];
-            }
-        }
+        //Valor de Debitos a pagar atrasados
+        $debitosPagarAtrasados =  DB::table('parcela_conta_pagar')
+        ->whereDate('data_vencimento', '<', $hoje)
+        ->where('situacao', 0)
+        ->where('debito_id', '!=', null)
+        ->sum('valor_parcela');
 
-        //Total de titulares
-        $total_titular_conta = $titulares_contas->count();
-    
+        //Valor de Debitos a receber atrasados
+        $debitosReceberAtrasados =  DB::table('parcela_conta_receber')
+        ->whereDate('data_vencimento', '<', $hoje)
+        ->where('situacao', 0)
+        ->where('debito_id', '!=', null)
+        ->sum('valor_parcela');
 
-        //select referente a parcelas de contas a pagar de lotes
-        $queryDebitos = DB::table('parcela_conta_pagar as p')
-            ->selectRaw('SUM(p.valor_parcela) as total_debitos')
-            ->selectRaw('CASE WHEN c.razao_social IS NOT NULL THEN c.razao_social ELSE c.nome END AS nome_cliente_ou_razao_social')
-            ->join('debito as d', 'p.debito_id', '=', 'd.id')
-            ->join('lote as l', 'd.lote_id', '=', 'l.id')
-            ->join('cliente as c', 'l.cliente_id', '=', 'c.id')
-            ->join('titular_conta as tc', 'd.titular_conta_id', '=', 'tc.id')
-            ->whereNull('p.situacao')
-            ->groupBy('l.cliente_id', 'nome_cliente_ou_razao_social')
-            ->get();
-
-        //Consultar titulares de contas
-        $conta_pagar_titulares = $conta_pagar_titulares->get();
-
-        //Definir Empresa Principal responsável pelos débitos
-        foreach($conta_pagar_titulares as $titular_conta){
-            if($titular_conta->id == 1){
-                $titular_empresa_principal_id = $titular_conta->id;
-                $titular_empresa_principal_nome = $titular_conta->nome_cliente_ou_razao_social;
-            }
-        }
-
-        $debitosEmpresa = [];
-        $debitosValorClientes = 0;
-
-        //Dividir em Débitos de Empresa e Clientes
-        foreach ($queryDebitos as $debitos) {
-            if(isset($debitos->nome_cliente_ou_razao_social) && isset($titular_empresa_principal_nome)){
-                if ($debitos->nome_cliente_ou_razao_social == $titular_empresa_principal_nome) {
-                    $debitosEmpresa = [
-                        'total_debitos' => $debitos->total_debitos,
-                        'nome_cliente_ou_razao_social' => $debitos->nome_cliente_ou_razao_social,
-                    ];
-        
-                }
-            } else {
-                $debitosValorClientes += $debitos->total_debitos;
-            }
-        }
-
-        //Total Débitos de Clientes
-        $debitosClientes = [
-            'total_debitos' => $debitosValorClientes,
-            'nome_cliente_ou_razao_social' => 'CLIENTES',
-
-        ];
-
-        //Unindo os dois resultados
-        $debitosEmpresaCliente = [
-            $debitosClientes,
-            $debitosEmpresa,
-        ];
+        //Valor total de debitos atrasados a receber e pagar
+        $totalDividaDebitos = $debitosPagarAtrasados + $debitosReceberAtrasados;
 
         //Alimentar gráfico de Receber Débitos por ano
         $receberPorAnos = DB::table('parcela_conta_receber as p')
@@ -153,64 +72,12 @@ class DashboardController extends Controller
             ->orderBy('ano_vencimento', 'ASC')
             ->get();
 
-        //Alimentar gráfico de Receber Débitos por ano
-        $lotesEmpreendimentos = DB::table('empreendimento as e')
-            ->select(
-                'e.nome as empreendimento', 
-                DB::raw('COUNT(l.id) as total_lotes')
-            )
-            ->join('quadra as q', 'e.id', '=', 'q.empreendimento_id')
-            ->join('lote as l', 'q.id', '=', 'l.quadra_id')
-            ->join('debito as d', 'l.id', '=', 'd.lote_id')
-            ->groupBy('empreendimento')
-            ->orderBy('empreendimento', 'ASC')
-            ->get();
-
-
-        /*$lotesAjuizadosPorEmpreendimento = DB::table('debito as d')
-            ->select(
-                'e.nome as empreendimento', 
-                DB::raw('COUNT(DISTINCT l.id) as total_lotes_ajuizados'),
-                DB::raw('
-                    (SELECT COUNT(l.id)
-                    FROM empreendimento as e JOIN quadra as q ON e.id = q.empreendimento_id 
-                    JOIN lote as l ON q.id = l.quadra_id 
-                    group by e.nome
-                    order by e.nome ASC ) as total_lotes')
-            )
-            ->join('lote as l', 'd.lote_id', '=', 'l.id')
-            ->join('quadra as q', 'l.quadra_id', '=', 'q.id')
-            ->join('empreendimento as e', 'e.id', '=', 'q.empreendimento_id')
-            ->where('d.tipo_debito_id', 1)
-            ->groupBy('e.nome')
-            ->get();*/
-
-            $tipo_debitos = TipoDebito::limit(4)->get();
-
-            $lotesEmpreendimentos = DB::table('empreendimento as e')
-            ->select(
-                'e.nome as empreendimento',
-                DB::raw('COUNT(DISTINCT l.id) as total_lotes'),
-                DB::raw('COUNT(DISTINCT CASE WHEN d.tipo_debito_id = 1 THEN l.id END) as total_lotes_1'),
-                DB::raw('COUNT(DISTINCT CASE WHEN d.tipo_debito_id = 2 THEN l.id END) as total_lotes_2'),
-                DB::raw('COUNT(DISTINCT CASE WHEN d.tipo_debito_id = 3 THEN l.id END) as total_lotes_3'),
-                DB::raw('COUNT(DISTINCT CASE WHEN d.tipo_debito_id = 4 THEN l.id END) as total_lotes_4'),
-            )
-            ->join('quadra as q', 'e.id', '=', 'q.empreendimento_id')
-            ->join('lote as l', 'q.id', '=', 'l.quadra_id')
-            ->leftJoin('debito as d', 'l.id', '=', 'd.lote_id')
-            ->leftJoin('parcela_conta_receber as p', 'd.id', '=', 'p.debito_id')
-            ->groupBy('e.nome')
-            ->orderBy('e.nome', 'ASC')
-            ->get();
-
         $data = [
-            'total_titular_conta' => $total_titular_conta,
-            'titulares_contas' => $data,
-            'debitosEmpresaCliente' => $debitosEmpresaCliente,
+            'pagarHoje' => $pagarHoje,
+            'receberHoje' => $receberHoje,
+            'pagamentosAtrasados' => $pagamentosAtrasados,
+            'totalDividaDebitos' => $totalDividaDebitos,
             'receberPorAnos' => $receberPorAnos->toArray(),
-            'lotesEmpreendimentos' => $lotesEmpreendimentos,
-            'tipo_debitos' => $tipo_debitos
         ];
 
 
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,7 +12,7 @@
                 </div>
                 <div class="col-md-8 align-self-center">
                     <h3>Contas a Pagar Hoje</h3>
-                    <p>R$ 2.352,12</p>
+                    <p>R$ {{number_format($data['pagarHoje'], 2, ',', '.')}}</p>
                 </div>
             </div>
         </div>
@@ -27,7 +27,7 @@
                 </div>
                 <div class="col-md-8 align-self-center">
                     <h3>Contas a Receber Hoje</h3>
-                    <p>R$ 2.352,12</p>
+                    <p>R$ {{number_format($data['receberHoje'], 2, ',', '.')}}</p>
                 </div>
             </div>
         </div>
@@ -41,8 +41,8 @@
                     </span>
                 </div>
                 <div class="col-md-8 align-self-center">
-                    <h3>Pagamentos Atrasados</h3>
-                    <p>R$ 2.352,12</p>
+                    <h3>Total de Parcelas Atrasadas</h3>
+                    <p>R$ {{number_format($data['pagamentosAtrasados'], 2, ',', '.')}}</p>
                 </div>
             </div>
         </div>
@@ -56,114 +56,12 @@
                     </span>
                 </div>
                 <div class="col-md-8 align-self-center">
-                    <h3>Total de Débitos de IPTUs</h3>
-                    <p>R$ 2.352,12</p>
+                    <h3>Total de Débitos Atrasados</h3>
+                    <p>R$ {{number_format($data['totalDividaDebitos'], 2, ',', '.')}}</p>
                 </div>
             </div>
         </div>
     </div>    
-</div>
-
-<div class="row">
-    <div class="col-md-8">
-        <div class="card">
-            <h5 class="card-header">Débitos a receber de clientes</h5>
-            <div class="card-body">
-                <canvas id="graficoReceberDebitos"></canvas>
-            </div>
-        </div>
-    </div>
-    <div class="col-md-4">
-        <div class="card">
-            <div class="card-ranking">
-                <h4>Ranking Média Maiores Saídas por Categoria ao Mês</h4>
-                <div>
-                    <div class="linha-ranking">
-                        <p>Pagamento de Exemplo</p>
-                        <div class="progress" role="progressbar" aria-label="Basic example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
-                            <div class="progress-bar" style="width: 75%">18.000,00/mês</div>
-                        </div>
-                    </div>
-                    <div class="linha-ranking">
-                        <p>Pagamento de Exemplo</p>
-                        <div class="progress" role="progressbar" aria-label="Basic example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
-                            <div class="progress-bar" style="width: 40%">11.000,00/mês</div>
-                        </div>
-                    </div><div class="linha-ranking">
-                        <p>Pagamento de Exemplo</p>
-                        <div class="progress" role="progressbar" aria-label="Basic example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
-                            <div class="progress-bar" style="width: 25%">6.080,00/mês</div>
-                        </div>
-                    </div><div class="linha-ranking">
-                        <p>Pagamento de Exemplo</p>
-                        <div class="progress" role="progressbar" aria-label="Basic example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
-                            <div class="progress-bar" style="width: 7%">2.000,00/mês</div>
-                        </div>
-                    </div><div class="linha-ranking">
-                        <p>Pagamento de Exemplo</p>
-                        <div class="progress" role="progressbar" aria-label="Basic example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
-                            <div class="progress-bar" style="width: 2%">760,00/mês</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
-@if(isset($data['tipo_debitos'][0]))
-<div class="row">
-    <div class="col">
-        <div class="card">
-            <h5 class="card-header">Lotes</h5>
-            <div class="card-body">
-                <table class="table">
-                    <thead>
-                        <tr class="text-left">
-                            <th scope="col">Empreendimento</th>
-                            <th scope="col">Qtnd. de lotes {{$data['tipo_debitos'][0]->descricao}}</th>
-                            <th scope="col">Qtnd. de lotes {{$data['tipo_debitos'][1]->descricao}}</th>
-                            <th scope="col">Qtnd. de lotes {{$data['tipo_debitos'][2]->descricao}}</th>
-                            <th scope="col">Qtnd. de lotes {{$data['tipo_debitos'][3]->descricao}}</th>
-                            <th scope="col">Total Lotes</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    @foreach($data['lotesEmpreendimentos'] as $resultado)
-                        <tr class="resultados-table text-left">
-                            <td scope="row">{{$resultado->empreendimento}}</td>
-                            <td scope="row">{{$resultado->total_lotes_1}}</td>
-                            <td scope="row">{{$resultado->total_lotes_2}}</td>
-                            <td scope="row">{{$resultado->total_lotes_3}}</td>
-                            <td scope="row">{{$resultado->total_lotes_4}}</td>
-                            <td scope="row">{{ $resultado->total_lotes }}</td>
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </div>
-</div>
-@endif
-
-<div class="row">
-    <div class="col">
-        <div class="card">
-            <h5 class="card-header">Débitos referentes a Clientes e Empresa</h5>
-            <div class="card-body">
-                <canvas id="graficoDividaClienteEmpresa"></canvas>
-            </div>
-        </div>
-    </div>
-    <div class="col">
-        <div class="card">
-            <h5 class="card-header">Total de Contas a Pagar por titular (não incluso débitos de IPTU)</h5>
-            <div class="card-body">
-                <canvas id="graficoDebitosTitulares"></canvas>
-            </div>
-        </div>
-    </div>
 </div>
 
 
@@ -171,87 +69,7 @@
 <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels"></script>
 
 <script>
-// Obtém os dados do PHP e armazena em variáveis JavaScript
-const labels_graficoDebitosTitulares = {!!json_encode(array_column($data['titulares_contas'], 'nome_cliente_ou_razao_social')) !!};
-const data_graficoDebitosTitulares = {!!json_encode(array_column($data['titulares_contas'], 'total_contas_pagar')) !!};
 
-const labels_graficoDividaClienteEmpresa = {!!json_encode(array_column($data['debitosEmpresaCliente'], 'nome_cliente_ou_razao_social')) !!};
-const data_graficoDividaClienteEmpresa = {!!json_encode(array_column($data['debitosEmpresaCliente'], 'total_debitos')) !!};
-
-
-const labels_graficoReceberDebitos = {!!json_encode(array_column($data['receberPorAnos'], 'ano_vencimento')) !!};
-const data_graficoReceberDebitos = {!!json_encode(array_column($data['receberPorAnos'], 'total_debitos')) !!};
-
-const graficoDebitosTitulares = document.getElementById('graficoDebitosTitulares');
-const graficoDividaClienteEmpresa = document.getElementById('graficoDividaClienteEmpresa');
-const graficoReceberDebitos = document.getElementById('graficoReceberDebitos');
-
-
-
-new Chart(graficoDebitosTitulares, {
-    type: 'bar',
-    data: {
-        labels: labels_graficoDebitosTitulares,
-        datasets: [{
-            label: 'R$ Contas a Pagar',
-            data: data_graficoDebitosTitulares,
-            backgroundColor: [
-                'rgba(177, 7, 192, 0.8)',
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
-});
-
-new Chart(graficoDividaClienteEmpresa, {
-    type: 'pie',
-    data: {
-        labels: labels_graficoDividaClienteEmpresa,
-        datasets: [{
-            label: 'R$ Débitos',
-            data: data_graficoDividaClienteEmpresa,
-            backgroundColor: [
-                'rgb(54, 162, 235)',
-                'rgb(255, 99, 132)',
-            ],
-            hoverOffset: 2
-        }]
-    },
-    options: {
-        responsive: true,
-        maintainAspectRatio: false,
-    }
-});
-
-new Chart(graficoReceberDebitos, {
-    type: 'bar',
-    data: {
-        labels: labels_graficoReceberDebitos,
-        datasets: [{
-            label: 'R$ Débitos a receber',
-            data: data_graficoReceberDebitos,
-            backgroundColor: [
-                'RGBA(0, 139, 139)',
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        indexAxis: 'y',
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
-});
 </script>
 
 @endsection
