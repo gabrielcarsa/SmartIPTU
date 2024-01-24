dd8f2ece29a96487ba90b2d8618b58b39e23821b
Gabriel Cardoso <gabrielcarsa@hotmail.com>
2024-01-24 09:09:11 -0500

    arrumando scripts e importacoes do contas a receber/pagar

--- resources/views/conta_receber/contas_receber.blade.php
+++ resources/views/conta_receber/contas_receber.blade.php
@@ -372,9 +372,6 @@
 
 @endsection
 
-
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
 <script>
 function handleCheckboxClick(clickedCheckboxId) {
     // Desmarcar o outro checkbox
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -132,15 +132,19 @@
     @endif
 
     <main class="content">
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
         @yield('conteudo')
     </main>
 
     @stack('modals')
 
-    @livewireScripts
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
     </script>
+    @livewireScripts
+
 
 </body>
 
