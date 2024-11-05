82f130188ed4c0528cb54451d812a1ce0f041555
Gabriel Cardoso <gabrielcarsa@hotmail.com>
2024-11-05 08:07:09 -0500

    feat: verificar se ha escritura scrapping

--- resources/views/empreendimento/empreendimento_gestao.blade.php
+++ resources/views/empreendimento/empreendimento_gestao.blade.php
@@ -168,6 +168,11 @@
                             call
                         </span>
                         @endif
+                        @if($lote->is_escriturado == true)
+                        <span class="bg-success p-1 text-white rounded fw-bold">
+                            ESCR
+                        </span>
+                        @endif
                     </td>
                     @else
                     <td class="">
@@ -175,6 +180,11 @@
                         @if($lote->cliente->is_contato_verificado == true)
                         <span class="material-symbols-outlined bg-success rounded-circle text-white p-1 fs-6">
                             call
+                        </span>
+                        @endif
+                        @if($lote->is_escriturado == true)
+                        <span class="bg-success p-1 text-white rounded fw-bold">
+                            ESCR
                         </span>
                         @endif
                     </td>
